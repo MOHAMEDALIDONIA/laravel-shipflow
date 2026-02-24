07838a0aaf211996336dcdfedf0570e5f055be77
Mohamed Ali <mohamedalidonia11@gmail.com>
2026-02-24 12:18:47 +0200

    update composer 2

--- src/Console/Commands/PublishShippingConfig.php
+++ src/Console/Commands/PublishShippingConfig.php
@@ -14,6 +14,7 @@ class PublishShippingConfig extends Command
      */
     protected $signature = 'shipping:publish-config {--force : Overwrite existing config file}';
 
+    
     /**
      * The console command description.
      *
